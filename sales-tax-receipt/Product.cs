fe22929c673604b3eb5dcea22cf7bb62ad146fb3
SeannHewitson <seannkiely@icloud.com>
2020-12-15 00:13:03 +0000

    Initial commit

---
+++ sales-tax-receipt/Product.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sales_tax_receipt
+{
+    class Product
+    {
+        int quantity;
+        string name;
+        double price;
+        double tax;
+
+        public Product(string product)
+        {
+            string[] seperated = product.Split(' ');
+            //  Set Price
+            string split_price = seperated[seperated.Length - 1];
+            split_price = split_price.Replace("£", "");
+            double.TryParse(split_price, out price);
+
+            //  Set Quantity
+            int.TryParse(seperated[0], out quantity);
+
+            //  Set Name
+            name = "";
+            int max = seperated.Length - 2;
+            for (int i = 1; i <= max; i++)
+            {
+                name += seperated[i];
+                if(i != max)
+                {
+                    name += " ";
+                }
+            }
+
+            CalculateTax();
+        }
+
+        private void CalculateTax()
+        {
+            //  Get the percentage
+            double percentage = 0.00;
+            List<string> exemptions = new List<string>();
+            exemptions.Add("book");
+            exemptions.Add("chocolate");
+            exemptions.Add("pills");
+
+            //  Check if the product is imported
+            if (name.Contains("imported"))
+            {
+                percentage += 0.05;
+            }
+
+            //  Check if the product is not exempt
+            bool exempt = false;
+            foreach(string exemption in exemptions)
+            {
+                if (name.Contains(exemption))
+                {
+                    exempt = true;
+                }
+            }
+
+            if (!exempt)
+            {
+                percentage += 0.1;
+            }
+
+            //  Calculate the monetary value
+            double value = price * percentage;
+            tax = Math.Round(value * 20) / 20;
+        }
+
+        //  Getters
+        public int GetQuantity()
+        {
+            return quantity;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public double GetPrice()
+        {
+            return price;
+        }
+
+        public double GetTax()
+        {
+            return tax;
+        }
+    }
+}
---
+++ sales-tax-receipt/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sales_tax_receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a looped input
+            PrintInstructions();
+            string input = Console.ReadLine();
+            List<Product> basket = new List<Product>();
+
+            while (input != "quit")
+            {
+                //  Accept empty string or double return to print receipt.
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    PrintReceipt(basket);
+                    basket.Clear();
+                }
+                else
+                {
+                    Product product = new Product(input);
+                    basket.Add(product);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        static void PrintInstructions()
+        {
+            Console.WriteLine("Input a product, followed by the enter/return key.");
+            Console.WriteLine("Hit enter/return again to print the receipt.");
+            Console.WriteLine("To quit at any time, type quit, followed by the enter/return key.");
+            Console.WriteLine("Enter an item to begin:");
+        }
+
+        static void PrintReceipt(List<Product> basket)
+        {
+            double taxes = 0.00;
+            double total = 0.00;
+            //  Print each product in the basket.
+            foreach(Product product in basket)
+            {
+                int quantity = product.GetQuantity();
+                string name = product.GetName();
+                double price = product.GetPrice();
+                double tax = product.GetTax();
+                double sum_price = price + tax;
+                
+                taxes += tax;
+                total += sum_price;
+
+                string output = string.Format("{0} {1}: £{2}", quantity, name, sum_price);
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine(string.Format("Sales Taxes: £{0:F2}", taxes));
+            Console.WriteLine(string.Format("Total: £{0}", total));
+            Console.Write("\n\n");
+            PrintInstructions();
+        }
+    }
+}
